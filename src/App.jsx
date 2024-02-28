48aa86cee35b1481514f0ecec96883c85c0e3efd
ap-bhai <ap@easemyai.com>
2024-02-28 11:44:55 +0530

    allTested map exclueded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./style.css";
 import L from "leaflet";
-import MumbaiMap from "../Maps/mumbai.pmtiles";
 import IndiaMap from "../Maps/india_data.pmtiles";
 import Marker_icon_2x from "./assets/Marker/marker-icon-2x.png";
 import "./leaflet.1.9.4.css";
@@ -18,12 +17,94 @@ let config = {
   zoom: 14,
 };
 
+// mumbai data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "mumbai.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [19.0760, 72.8777],
+//   // loc: [19.302344, 73.055957],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 14,
+// };
+// mumbai data end
+
+// srilanka data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "srilanka.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [7.8731, 80.7718],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 8,
+// };
+// srilanka data end
+
+// bahrain data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "bahrain.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [26.0414, 50.5581],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 8,
+// };
+// bahrain data end
+
+// dubai data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "dubai.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [25.0962, 55.2111],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 8,
+// };
+// dubai data end
+
+// japan data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "japan_southkorea.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [25.0962, 55.2111],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 8,
+// };
+// japan data end
+
+// south korea data start
+// let config = {
+//   // The `pmtiles` file you extracted
+//   file: "maharashtra.pmtiles",
+//   // The center that should be shown on the map. [lat, lon] format
+//   loc: [19.0760, 72.8777],
+//   // How much to be zoomed in at the start (higher number = more zoomed in)
+//   zoom: 8,
+// };
+// south korea data end
+
 function App() {
+  // Tell Leaflet to create a map
   const map = L.map("map");
 
   console.log("map", map);
-  // console.log(L.tileLayer())
+
+  // Tell Leaflet where to center
   map.setView(config.loc, config.zoom);
+
+  // map.on('click', function(e) {
+  //     alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+  // });
+
+  // map.on('click', function(e) {
+  //     alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+  //     L.marker([event.latlng.lat , event.latlng.lng], { icon: greenIcon }).addTo(map);
+  // });
+
+  // map.on('click', (event)=> {
+  //     L.marker([event.latlng.lat , event.latlng.lng], { icon: greenIcon }).addTo(map);
+  // })
 
   let marker = null;
   map.on("click", (event) => {
@@ -35,7 +116,6 @@ function App() {
       icon: greenIcon,
     }).addTo(map);
   });
-
 
   // var LeafIcon = L.Icon.extend({
   //   options: {
@@ -69,7 +149,48 @@ function App() {
     .bindPopup("<b>Hello world!</b><br />I am a popup in India")
     .openPopup();
 
-  console.log(leafletLayer,"proto" )
+  // srilanka map point
+  // L.marker([7.8731, 80.7718], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in Srilanka").openPopup();
+
+  // bahrain map point
+  // L.marker([26.0414, 50.5581], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in Bahrain").openPopup();
+
+  // dubai map point
+  // L.marker([25.0962, 55.2111], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in dubai").openPopup();
+
+  // japan map point
+  // L.marker([35.6895, 139.6917], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in japan").openPopup();
+
+  // southkorea map point
+  // L.marker([36.642, 127.991], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in southkorea").openPopup();
+
+  // India state:
+  // Maharashtra map point
+  // L.marker([19.0760, 72.8777], { icon: greenIcon }).addTo(map)
+  //   .bindPopup("<b>Hello world!</b><br />I am a popup in maharashtra india").openPopup();
+  // var LeafIcon_2 = L.Icon.extend({
+  //     options: {
+
+  //         iconSize: [50, 82],
+  //         shadowSize: [50, 64],
+  //         iconAnchor: [22, 94],
+  //         shadowAnchor: [4, 62],
+  //         popupAnchor: [-3, -76]
+  //     }
+  // });
+
+  // var markers_icon = new LeafIcon_2 ({ iconUrl: 'Marker/marker-icon-2x.png' });
+
+  // L.marker([-26.64991293944776, 153.06281312253793], { icon: markers_icon }).addTo(map)
+  //     .bindPopup("<b>Hello world !</b><br />I am a popup in AUST.").openPopup();
+
+  console.log(leafletLayer, "proto");
+  // Tell Protomaps to create a layer for leaflet with import { leafletLayer } from "protomaps"
 
   var layer = leafletLayer({
     url: config.file,
@@ -87,7 +208,10 @@ function App() {
 
   layer.addTo(map);
 
-  return <div> </div>;
+  // Run this to enable clicking on the map and seeing the exact data that
+  // was in the `pmtiles` file for that location.
+  // layer.addInspector(map);
+
 }
 
 export default App;
